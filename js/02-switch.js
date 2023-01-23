2a66e9bc24be7097a3e08dd71d550bff4f9eea30
Iryna <irasokolova25@ukr.net>
2023-01-23 12:34:27 +0200

    add examples

--- js/02-switch.js
+++ js/02-switch.js
@@ -1,59 +1,84 @@
 /*
  * Напиши скрипт вибору готеля по кількості зірок.
- * 1 - 20$, 2 - 30$, 3 - 50$, 4 - 70$, 5 - 120$
+ * 1,2 - 20$,  3,4 - 50$,  5 - 120$
  *
  * Якщо в змінній stars щось окрім чисел 1-5, виведи рядок
  * 'Токої кількості зірок немає'
  */
 
-// const stars = 3;
-// let price;
+const stars = 4;
+let price;
 
-// if(stars === 1 && stars === 2){
-//     price = 20;
-//     console.log(`Ціна готелю з ${stars} зірками - ${price}$` );
-// }else if(stars === 2){
-//     price = 30;
-//     console.log(`Ціна готелю з ${stars} зірками - ${price}$` );
+// if(stars === 1){
+//     price = '20$'
+//     console.log(`Вартість номеру ${price}`);
+// } else if(stars === 2){
+//     price = '30$'
+//     console.log(`Вартість номеру ${price}`);
 // }else if(stars === 3){
-//     price = 50;
-//     console.log(`Ціна готелю з ${stars} зірками - ${price}$` );
+//     price = '50$'
+//     console.log(`Вартість номеру ${price}`);
 // }else if(stars === 4){
-//     price = 70;
-//     console.log(`Ціна готелю з ${stars} зірками - ${price}$` );
+//     price = '70$'
+//     console.log(`Вартість номеру ${price}`);
 // }else if(stars === 5){
-//     price = 120;
-//     console.log(`Ціна готелю з ${stars} зірками - ${price}$` );
-// }else{
+//     price = '120$'
+//     console.log(`Вартість номеру ${price}`);
+// }else {
 //     console.log('Токої кількості зірок немає');
 // }
 
 // switch (stars) {
 //     case 1:
-//         price = 20;
-//         console.log(`Ціна готелю з ${stars} зірками - ${price}$`);
-//         break;
 //     case 2:
-//         price = 30;
-//         console.log(`Ціна готелю з ${stars} зірками - ${price}$`);
+//         price = '20$'
+//         console.log(`Вартість номеру ${price}`);
 //         break;
-//     case 3:
-//         price = 50;
-//         console.log(`Ціна готелю з ${stars} зірками - ${price}$`);
+//     case 3: 
+//     case 4: 
+//         price = '50$'
+//         console.log(`Вартість номеру ${price}`);
 //         break;
-//     case 4:
-//         price = 70;
-//         console.log(`Ціна готелю з ${stars} зірками - ${price}$`);
-//         break;
-//     case 5:
-//         price = 120;
-//         console.log(`Ціна готелю з ${stars} зірками - ${price}$`);
-//         break;
-
+//     case 5: 
+//         price = '120$'
+//         console.log(`Вартість номеру ${price}`);
+//         break;  
+        
 //     default:
 //         console.log('Токої кількості зірок немає');
 //         break;
 // }
+
+// if (умова) {
+//     тіло
+// }
+
+function spinWords(string){
+    console.log(string.split(' '));
+    return string.split(' ').map(word => {
+        console.log(word);
+       if(word.length >= 5){
+        console.log(word);
+       console.log(word.split('').reverse().join(''));;
+      
+       }
+       console.log(word);
+       return word;
+     }).join(' ')
+   }
+   console.log(spinWords("Welcome"));
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  * Напиши скрипт вибору готеля по кількості зірок.
@@ -99,22 +124,22 @@ let message = '';
 
 // 2. робимо світч
 // 3. перевіряємо на рівність опцію і виводимо повідоилення
-switch (option) {
-    case 1:      
-        message = 'Ви зможете забрати товар завтра з 12:00 в нашому офісі'
-        console.log(message);
-        break;
-    case 2:
-        message = 'Кур"єр доставить замовлення завтра з 9:00 до 18:00'
-        console.log(message);
-        break;
-    case 3:
-        message = 'Посилка буде відправлена сьогодні';
-        console.log(message);
-        break;
-// 4. дефолтне повідомлення
-    default:
-        message = 'Вам передзвонит менеджер';
-        console.log(message);
-        break;
-}
+// switch (option) {
+//     case 1:      
+//         message = 'Ви зможете забрати товар завтра з 12:00 в нашому офісі'
+//         console.log(message);
+//         break;
+//     case 2:
+//         message = 'Кур"єр доставить замовлення завтра з 9:00 до 18:00'
+//         console.log(message);
+//         break;
+//     case 3:
+//         message = 'Посилка буде відправлена сьогодні';
+//         console.log(message);
+//         break;
+// // 4. дефолтне повідомлення
+//     default:
+//         message = 'Вам передзвонит менеджер';
+//         console.log(message);
+//         break;
+// }
