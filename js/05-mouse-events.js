d40efb731410481cfa7b5380d14978e014390920
Iryna <irasokolova25@ukr.net>
2023-04-14 15:05:27 +0300

    додає приклади заняття

--- js/05-mouse-events.js
+++ js/05-mouse-events.js
@@ -8,10 +8,10 @@
  */
 
 const boxRef = document.querySelector('.js-box');
-
+const innerBoxRef = document.querySelector('.inner-box')
 // boxRef.addEventListener('mouseover', onMouseEnter);
-// boxRef.addEventListener('mouseout', onMouseLeave);
-// boxRef.addEventListener('mousemove', onMouseMove);
+// boxRef.addEventListener('mouseleave', onMouseLeave);
+boxRef.addEventListener('mousemove', onMouseMove);
 
 function onMouseEnter(event) {
   const box = event.currentTarget;
@@ -19,10 +19,24 @@ function onMouseEnter(event) {
 }
 
 function onMouseLeave(event) {
+  
   const box = event.currentTarget;
   box.classList.remove('box--active');
 }
 
-function onMouseMove(event) {
-  console.log(event);
+function onMouseMove(e) {
+   // Отримуємо координати миші на екрані
+   const mouseX = e.pageX;
+   const mouseY = e.pageY;
+ 
+   // Отримуємо координати центра блоку
+   const innerBoxRefX = innerBoxRef.offsetLeft + innerBoxRef.offsetWidth / 2;
+   const innerBoxRefY = innerBoxRef.offsetTop + innerBoxRef.offsetHeight / 2;
+ 
+   // Обчислюємо відстань від миші до центру блоку
+   const distanceX = mouseX - innerBoxRefX;
+   const distanceY = mouseY - innerBoxRefY;
+ 
+   // Рухаємо блок на відстань, яку миша змістилася відносно центру блоку
+   innerBoxRef.style.transform = `translate(${distanceX}px, ${distanceY}px)`;
 }
--- js/06-modal.js
+++ js/06-modal.js
@@ -59,6 +59,15 @@
 
 
 
-
+  document.addEventListener("keydown", event => {
+    event.preventDefault();
+   
+    if ((event.ctrlKey || event.metaKey) && event.code === "KeyS") {
+      console.log("«Ctrl + s» or «Command + s» combo");
+    }
+   });
+   
+   
+   
   
  
