b5bbe19f8c1620c40d86638d01f6e4e321e1c4d5
Iryna <irasokolova25@ukr.net>
2023-04-15 14:11:58 +0300

    додає приклади

--- js/03-class.js
+++ js/03-class.js
@@ -9,6 +9,50 @@
  * 🐷 - Геттери та сеттери
  */
 
+class Car {
+    constructor(model, price, year, color){
+        this._model = model
+        this._price = price
+        this.year = year
+        this.color = color
+    }
+
+    get model(){
+        console.log(this._model);
+    }
+
+    set price(newPrice){
+        this._price = newPrice;
+    }
+    // getModel(){
+    //     console.log(this.model);
+    // }
+
+    // changePrice(newPrice){
+    //     this.price = newPrice;
+    // }
+}
+
+const audi = new Car('A5', 40000, 2020, 'black');
+audi.model;
+audi.price = 50000;
+// audi.getModel();
+// audi.changePrice(45000);
+console.log(audi);
+
+const mazda  = new Car('mazda-3', 35000, 2022, 'red');
+// mazda.getModel();
+// mazda.changePrice(40000);
+// console.log(mazda);
+
+
+
+
+
+
+
+// 'A5', 40000, 2020, 'black'
+// 'mazda-3', 35000, 2022, 'red'
 
 // function Car (model, price, year, color){
 //     this.model = model;
@@ -31,31 +75,3 @@
 
 // const mazda = new Car('mazda-3', 35000, 2022, 'red');
 // console.log(mazda);
-
-
-class Car {
-    static description = 'Це функція конструктор!'
-
-    constructor(model, price, year, color){
-        this.model = model;
-        this._price = price;
-        this.year = year;
-        this.color = color;
-    }
-
-    get price(){
-        this._price;
-    }
-
-    set price(newPrice){
-        this._price = newPrice;
-    }
-}
-// const audi = new Car('A5', 40000, 2020, 'black');
-// console.log(audi);
-// console.log(audi.price);
-// console.log(audi.price(38000));
-
-
-const mazda = new Car('mazda-3', 35000, 2022, 'red');
-console.log(mazda);
