71af481f01dcbec48ddac5c82efd6796c0934c57
Iryna <irasokolova25@ukr.net>
2023-07-08 00:56:50 +0300

    ready

--- src/js/03-colorpicker.js
+++ src/js/03-colorpicker.js
@@ -1,50 +1,48 @@
-import colorCardTpl from '../templates/color-card.hbs';
-import colorCardsTpl from '../templates/color-cards.hbs';
-import colors from './colors.json';
+
 import '../css/common.css';
 import '../css/colorpicker.css';
 
-const paletteContainer = document.querySelector('.js-palette');
-const cardsMarkup = createColorCardsMarkup(colors);
+// const paletteContainer = document.querySelector('.js-palette');
+// const cardsMarkup = createColorCardsMarkup(colors);
 
-paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-paletteContainer.addEventListener('click', onPaletteContainerClick);
+// paletteContainer.addEventListener('click', onPaletteContainerClick);
 
-function createColorCardsMarkup(colors) {
-  // return colors.map(color => colorCardTpl(color)).join('');
-  // return colors.map(colorCardTpl).join('');
+// function createColorCardsMarkup(colors) {
+//   // return colors.map(color => colorCardTpl(color)).join('');
+//   // return colors.map(colorCardTpl).join('');
 
-  return colorCardsTpl(colors);
-}
+//   return colorCardsTpl(colors);
+// }
 
-function onPaletteContainerClick(evt) {
-  const isColorSwatchEl = evt.target.classList.contains('color-swatch');
+// function onPaletteContainerClick(evt) {
+//   const isColorSwatchEl = evt.target.classList.contains('color-swatch');
 
-  if (!isColorSwatchEl) {
-    return;
-  }
+//   if (!isColorSwatchEl) {
+//     return;
+//   }
 
-  const swatchEl = evt.target;
-  const parentColorCard = swatchEl.closest('.color-card');
+//   const swatchEl = evt.target;
+//   const parentColorCard = swatchEl.closest('.color-card');
 
-  removeActiveCardClass();
-  addActiveCardClass(parentColorCard);
-  setBodyBgColor(swatchEl.dataset.hex);
-}
+//   removeActiveCardClass();
+//   addActiveCardClass(parentColorCard);
+//   setBodyBgColor(swatchEl.dataset.hex);
+// }
 
-function setBodyBgColor(color) {
-  document.body.style.backgroundColor = color;
-}
+// function setBodyBgColor(color) {
+//   document.body.style.backgroundColor = color;
+// }
 
-function removeActiveCardClass() {
-  const currentActiveCard = document.querySelector('.color-card.is-active');
+// function removeActiveCardClass() {
+//   const currentActiveCard = document.querySelector('.color-card.is-active');
 
-  if (currentActiveCard) {
-    currentActiveCard.classList.remove('is-active');
-  }
-}
+//   if (currentActiveCard) {
+//     currentActiveCard.classList.remove('is-active');
+//   }
+// }
 
-function addActiveCardClass(card) {
-  card.classList.add('is-active');
-}
+// function addActiveCardClass(card) {
+//   card.classList.add('is-active');
+// }
