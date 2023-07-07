9beee9c27fc0cdf27829800c704d293e1500ec1a
Iryna <irasokolova25@ukr.net>
2023-07-07 23:59:20 +0300

    add files

---
+++ src/js/04-gallery.js
@@ -0,0 +1,10 @@
+import galleryItemTpl from '../templates/gallery-item';
+import countries from './countries.json';
+import '../css/common.css';
+import '../css/gallery.css';
+
+const galleryRef = document.querySelector('.js-gallery');
+
+const markup = galleryItemTpl(countries);
+
+galleryRef.insertAdjacentHTML('beforeend', markup)
