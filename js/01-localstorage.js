e662a94d75b5da8eb23c65c5ef8ec95962ecaa7e
Iryna <irasokolova25@ukr.net>
2023-07-21 22:53:31 +0300

    додає приклади

--- js/01-localstorage.js
+++ js/01-localstorage.js
@@ -4,3 +4,17 @@
  * 
  */
 
+const student={
+    name: "Viktor",
+    age:19,
+    location: 'Kharkiv'
+}
+
+console.log(localStorage);
+
+localStorage.setItem("student", JSON.stringify(student))
+
+const storageKey = localStorage.getItem("student")
+console.log(JSON.parse(storageKey));
+
+// localStorage.removeItem("data")
--- js/02-feedback.js
+++ js/02-feedback.js
@@ -1,5 +1,5 @@
-import '../css/common.css';
-import '../css/feedback-form.css';
+// import '../css/common.css';
+// import '../css/feedback-form.css';
 
 /**
  * Створюємо доступ до елементів
@@ -8,17 +8,41 @@ import '../css/feedback-form.css';
 const formRef = document.querySelector('.js-feedback-form');
 const textareaRef = document.querySelector('.js-feedback-form textarea');
 
-formRef.addEventListener('submit', handleFormSubmit);
-textareaRef.addEventListener('input', handleTextareaInput);
+// formRef.addEventListener('submit', handleFormSubmit);
+// textareaRef.addEventListener('input', handleTextareaInput);
+formRef.addEventListener('input', handleInputValue);
 
-getValueFromLocalStorage();
+const formData = {}
+
+function handleInputValue(e){
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value;
+    
+    localStorage.setItem('formData', JSON.stringify(formData))
+}
+
+
+
+// obj.name
+// obj['name']
+
+
+// getValueFromLocalStorage();
+
 /*
  * - Зупиняєм поведінку за замовчуванням
  * - Видаляємо повідомлення зі сховища
  * - Очищуємо форму
  */
-function handleFormSubmit(event) {
-}
+// function handleFormSubmit(event) {
+//    event.preventDefault();
+
+//    event.currentTarget.reset();
+//    localStorage.removeItem('value');
+
+// }
 
 
 /*
@@ -26,20 +50,34 @@ function handleFormSubmit(event) {
  * - Зберігаємо його у сховищі
  * - Можно додати throttle
  */
-function handleTextareaInput(event) {
-   
-}
+// function handleTextareaInput(event) {
+//     console.log(event.target.value);
+//     const value = event.target.value;
+//     localStorage.setItem('value', value);
+// }
 /*
  * - Отримуємо значення зі сховища
  * - Якщо там щось було, оновлюємо DOM
  */
-function getValueFromLocalStorage() {
-   
+// function getValueFromLocalStorage() {
+//   const comment = localStorage.getItem("value");
     
-}
+//   if(comment){
+//     textareaRef.textContent = comment;
+//   }
+      
+// }
 
 // ДОДАТКОВО
-// зробити так щоб зберігало не тільки повідомлення, але і ім'я і все в одному об'єкті
+// зробити так щоб зберігало не тільки повідомлення, 
+// але і ім'я і все в одному об'єкті
+
+
+
+
+
+
+
 
 // const formData = {};
 
